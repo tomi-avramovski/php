e663837ab675c183b6083914a3662553f8bfb4ca
zeratul94 <tomi.avramovski@gmail.com>
2018-05-04 08:58:18 +0200

    Add files via upload

---
+++ categories.php
@@ -0,0 +1,30 @@
+<?php
+
+include('../models/categories.php');
+
+$isValid = 	isset($_POST['category'])
+			&& $_POST['category'] != '';
+			
+if($isValid) {
+	$data = ['category' => $_POST['category']];
+	add_blog_categories($data);
+}
+$res = get_blog_categories();
+
+update_blog_categories();
+delete_blog_categories();
+?>
+
+<table border="1" width="70%">
+	<tr>
+		<th>id</th>
+		<th>category</th>
+	</tr>
+		<?php foreach($res as $row){ ?>
+		<tr>
+			<td><?=$row['id']; ?></td>
+			<td><?=$row['category']; ?></td>
+			
+		</tr>
+	<?php } ?>
+</table>
